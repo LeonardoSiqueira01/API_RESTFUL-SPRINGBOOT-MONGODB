5864889ba3bffc20ba67dffd64638a49d2a63664
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2023-07-18 11:09:37 -0300

    getting user by id and handling exceptions

--- src/main/java/com/leonardoSiqueira/WorkshopMongoDB/services/UserService.java
+++ src/main/java/com/leonardoSiqueira/WorkshopMongoDB/services/UserService.java
@@ -1,20 +1,29 @@
 package com.leonardoSiqueira.WorkshopMongoDB.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leonardoSiqueira.WorkshopMongoDB.domain.User;
 import com.leonardoSiqueira.WorkshopMongoDB.repository.UserRepository;
+import com.leonardoSiqueira.WorkshopMongoDB.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado!"));
+
+	}
+
 }
