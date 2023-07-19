4bfbb447a965cc2f53ce7b08f4d99fdc13433e39
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2023-07-19 13:27:55 -0300

    Getting a post by id

---
+++ src/main/java/com/leonardoSiqueira/WorkshopMongoDB/services/PostService.java
@@ -0,0 +1,26 @@
+package com.leonardoSiqueira.WorkshopMongoDB.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leonardoSiqueira.WorkshopMongoDB.domain.Post;
+import com.leonardoSiqueira.WorkshopMongoDB.repository.PostRepository;
+import com.leonardoSiqueira.WorkshopMongoDB.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+
+	public Post findById(String id) {
+	    Optional<Post> user = repo.findById(id);
+	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}	
+
+	
+}
